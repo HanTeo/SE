3b8bb995da1fd6e096b5689897eeeda6742cd3f8
hanwei <hanwei.teo@gmail.com>
2017-01-18 15:52:51 +0000

    with Google docs data

--- SE.Tests/StatisticsCalculatorTests.cs
+++ SE.Tests/StatisticsCalculatorTests.cs
@@ -52,5 +52,53 @@ namespace SE.Tests
             Assert.AreEqual(2.5, variance);
             Assert.AreEqual(1.5811, stdDev, 0.001);
         }
+
+        [TestMethod]
+        public void TestWithDataProviderOnline()
+        {
+            var items = TestDataProvider.GetCsvData("0B-zod47PgWfmZ2ZQWmZRMXVyNXc");
+
+            double mean;
+            double variance;
+            double stdDev;
+            var pass = items.TryCalculateOnline(out mean, out variance, out stdDev);
+
+            Assert.IsTrue(pass);
+            Assert.AreEqual(50.344, mean, 0.001);
+            Assert.AreEqual(830.231, variance,0.001);
+            Assert.AreEqual(28.814, stdDev, 0.001);
+        }
+
+        [TestMethod]
+        public void TestWithDataProviderTwoPass()
+        {
+            var items = TestDataProvider.GetCsvData("0B-zod47PgWfmZ2ZQWmZRMXVyNXc");
+
+            double mean;
+            double variance;
+            double stdDev;
+            var pass = items.TryCalculateTwoPass(out mean, out variance, out stdDev);
+
+            Assert.IsTrue(pass);
+            Assert.AreEqual(50.344, mean, 0.001);
+            Assert.AreEqual(829.401, variance, 0.001);
+            Assert.AreEqual(28.799, stdDev, 0.001);
+        }
+
+        [TestMethod]
+        public void TestGetDataProviderOnePass()
+        {
+            var items = TestDataProvider.GetCsvData("0B-zod47PgWfmZ2ZQWmZRMXVyNXc");
+
+            double mean;
+            double variance;
+            double stdDev;
+            var pass = items.TryCalculateOnePass(out mean, out variance, out stdDev);
+
+            Assert.IsTrue(pass);
+            Assert.AreEqual(50.344, mean, 0.001);
+            Assert.AreEqual(829.401, variance, 0.001);
+            Assert.AreEqual(28.799, stdDev, 0.001);
+        }
     }
 }
---
+++ SE/TestDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace SE
+{
+    public class TestDataProvider
+    {
+        public static double[] GetCsvData(string id)
+        {
+            string url = $"https://drive.google.com/uc?export=download&id={id}";
+
+            using (var client = new WebClient())
+            {
+                var elements = client
+                    .DownloadString(url)
+                    .Split(',');
+
+                var numbers = Array.ConvertAll(elements, double.Parse);
+
+                return numbers;
+            }
+        }
+    }
+}
