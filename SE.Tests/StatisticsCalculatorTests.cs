a0b9da8ea9f4ce2268506f56d4d214c254afce7f
hanwei <hanwei.teo@gmail.com>
2017-01-18 16:02:02 +0000

    histogram

--- SE.Tests/StatisticsCalculatorTests.cs
+++ SE.Tests/StatisticsCalculatorTests.cs
@@ -100,5 +100,27 @@ namespace SE.Tests
             Assert.AreEqual(829.401, variance, 0.001);
             Assert.AreEqual(28.799, stdDev, 0.001);
         }
+
+        [TestMethod]
+        public void TestHistogram()
+        {
+            var items = new[] { 1.0, 19.9, 20.0, 21.0, 4.0, 5.0, 1.0, 4.0 };
+            var hist = new Histogram(0, 100, 10);
+
+            hist.Add(items);
+
+            CollectionAssert.AreEqual(new[] { 5, 1, 2, 0, 0, 0, 0, 0, 0, 0 }, hist.DataPoints);
+        }
+
+        [TestMethod]
+        public void TestHistogramWithProviderData()
+        {
+            var numbers = TestDataProvider.GetCsvData("0B-zod47PgWfmZ2ZQWmZRMXVyNXc");
+            var hist = new Histogram(0, 100, 10);
+
+            hist.Add(numbers);
+
+            CollectionAssert.AreEqual(new[] { 94, 107, 99, 94, 96, 101, 109, 101, 100, 99 }, hist.DataPoints);
+        }
     }
 }
--- SE/Histogram.cs
+++ SE/Histogram.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SE
 {
-    class Histogram
+    public class Histogram
     {
+        private int[] _freq;
+        private double _start;
+        private double _end;
+        private int _interval;
+
+        public Histogram(double start, double end, int buckets)
+        {
+            if (_start < _end)
+            {
+                throw new ArgumentOutOfRangeException($"End must be greater than start");
+            }
+
+            _start = start;
+            _end = end;
+            _interval = (int)(end - start) / buckets;
+            _freq = Enumerable.Repeat(0, buckets).ToArray();
+        }
+
+        public void Add(double number)
+        {
+            if (number < _start || number > _end)
+            {
+                throw new ArgumentOutOfRangeException($"Number is out of range {_start}..{_end}");
+            }
+
+            var i = (int)number / _interval;
+            _freq[i]++;
+        }
+
+        public void Add(double[] numbers)
+        {
+            foreach (var n in numbers)
+                Add(n);
+        }
+
+        public int[] DataPoints { get { return _freq; } }
     }
 }
